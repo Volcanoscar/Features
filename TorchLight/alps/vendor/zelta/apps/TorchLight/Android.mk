8312a4deeb9853f94dd8b1ddb2e73b24212c5a4f
chehongbin <635252544@qq.com>
2015-12-02 14:33:35 +0800

    chb modify for 设置TrochLight有效点击区域，并增加异常判定

--- TorchLight/alps/vendor/zelta/apps/TorchLight/Android.mk
+++ TorchLight/alps/vendor/zelta/apps/TorchLight/Android.mk
@@ -7,10 +7,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4 \
-    Baidu_MobAds_SDK
-#LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
 LOCAL_PACKAGE_NAME := TorchLight
 
@@ -24,7 +21,3 @@ include $(BUILD_PACKAGE)
 
 # This finds and builds the test apk as well, so a single make does both.
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := Baidu_MobAds_SDK:libs/Baidu_MobAds_SDK_3.2.jar
-include $(BUILD_MULTI_PREBUILT)
--- TorchLight/alps/vendor/zelta/apps/TorchLight/src/com/zxx1/light/LightActivity.java
+++ TorchLight/alps/vendor/zelta/apps/TorchLight/src/com/zxx1/light/LightActivity.java
@@ -15,115 +15,92 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.baidu.mobads.AdSettings;
-import com.baidu.mobads.AdView;
-import com.baidu.mobads.AdViewListener;
-
 public class LightActivity extends Activity {
-	private Button lightBtn = null;
-	private Camera camera = null;
-	private Parameters parameters = null;
-	public static boolean kaiguan = true; 
-	// public static boolean action = false;
+    private Button backBtn = null;
+    private Button btn = null;
+    private Camera camera = null;
+    private Parameters parameters = null;
+    public static boolean kaiguan = true;
 
-	private int back = 0;
-	AdView adView = null;
+    private int back = 0;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		adView = new AdView(this);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		requestWindowFeature(Window.FEATURE_NO_TITLE); 
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD,
-				WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		setContentView(R.layout.main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD,
+            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.main);
 
-		lightBtn = (Button) findViewById(R.id.btn_light);
-		lightBtn.setOnClickListener(new Mybutton());
-		addContentView(adView, new ViewGroup.LayoutParams(-1, -2));
+        backBtn = (Button) findViewById(R.id.btn_light);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new Mybutton());
+    }
 
-	}
+    class Mybutton implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+           if (kaiguan) {
+               try {
+                   camera = Camera.open();
+               } catch(Exception e) {
+                   Toast.makeText(LightActivity.this, "Please close Camera or TorchLight in other space!", Toast.LENGTH_SHORT).show();
+                   return ;
+               }
+               parameters = camera.getParameters();
+               parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+               camera.setParameters(parameters);
+               camera.startPreview();
+               backBtn.setBackgroundResource(R.drawable.shou_on);
+               kaiguan = false;
+           } else {
+               backBtn.setBackgroundResource(R.drawable.shou_off);
+               parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+               camera.setParameters(parameters);
+               camera.stopPreview();
+               kaiguan = true;
+               camera.release();
+           }
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            back++;
+            switch (back) {
+            case 1:
+                Toast.makeText(LightActivity.this,
+                    getString(R.string.again_exit), Toast.LENGTH_SHORT)
+                    .show();
+                break;
+            case 2:
+                back = 0;
+                Myback();
+                break;
+            }
+            return true;
+        } else {
+            return super.onKeyDown(keyCode, event);
+       }
+    }
 
-	class Mybutton implements OnClickListener {
-		@Override
-		public void onClick(View v) {
-			if (kaiguan) {
-
-				lightBtn.setBackgroundResource(R.drawable.shou_on);
-				camera = Camera.open();
-				parameters = camera.getParameters();
-				parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
-				camera.setParameters(parameters);
-				camera.startPreview();
-				kaiguan = false;
-			} else {
-				// addContentView(adView, new ViewGroup.LayoutParams(-1, -2));
-				lightBtn.setBackgroundResource(R.drawable.shou_off);
-				parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
-				camera.setParameters(parameters);
-				camera.stopPreview();
-				kaiguan = true;
-				camera.release();
-			}
-
-		}
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-	}
-
-	// @Override
-	// public boolean onCreateOptionsMenu(Menu menu) {
-	// menu.add(0, 2, 2, "exit");
-	// return super.onCreateOptionsMenu(menu);
-	// }
-	//
-	// @Override
-	// public boolean onOptionsItemSelected(MenuItem item) {
-	// switch (item.getItemId()) {
-	// case 2:
-	// Myback();
-	// break;
-	// }
-	// return super.onOptionsItemSelected(item);
-	// }
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			back++;
-			switch (back) {
-			case 1:
-				Toast.makeText(LightActivity.this,
-						getString(R.string.again_exit), Toast.LENGTH_SHORT)
-						.show();
-				break;
-			case 2:
-				back = 0;
-				Myback();
-				break;
-			}
-			return true;
-		} else {
-			return super.onKeyDown(keyCode, event);
-		}
-	}
-
-	public void Myback() { 
-		if (kaiguan) {
-			LightActivity.this.finish();
-			android.os.Process.killProcess(android.os.Process.myPid());
-		} else if (!kaiguan) {
-			camera.release();
-			LightActivity.this.finish();
-			android.os.Process.killProcess(android.os.Process.myPid());
-			kaiguan = true;
-		}
-	}
+    public void Myback() { 
+        if (kaiguan) {
+            LightActivity.this.finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
+        } else if (!kaiguan) {
+            camera.release();
+            LightActivity.this.finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            kaiguan = true;
+        }
+    }
 }
